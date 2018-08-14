eadb9df1922d53637ee35d7e29953f2b1ec290d9
miningful <newsunmin@naver.com>
2018-08-14 16:50:50 +0900

    vending machine 클래스 생성

--- test_vm.py
+++ test_vm.py
@@ -1,20 +1,21 @@
-from vm import run
-
-def init():
-    global change
-    change = 0
+from vm import run, init, VendingMachine
 
 def test_initial_change_should_be_zero():
-    init()
-    assert "잔액은 0원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "잔액은 0원입니다" == m.run("잔액")
+
+
+def test_initial_change_should_be_zero():
+    m = VendingMachine()
+    assert "잔액은 0원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
-    init()
-    assert "100원을 넣었습니다" == run("동전 100")
-    assert "잔액은 100원입니다" == run("잔액")
+    m = VendingMachine()
+    assert "100원을 넣었습니다" == m.run("동전 100")
+    assert "잔액은 100원입니다" == m.run("잔액")
 
 def test_insert_coin_and_check():
-    init()
+    m = VendingMachine()
     run("동전 100")
     run("동전 100")
-    assert "잔액은 200원입니다" == run("잔액")
+    assert "잔액은 200원입니다" == m.run("잔액")
