fa72c85277027089b77f5207fd7abc80f71893be
miningful <newsunmin@naver.com>
2018-08-20 12:06:09 +0900

    커피 추가하기

--- test_vm.py
+++ test_vm.py
@@ -16,6 +16,17 @@ def test_insert_coin_and_check():
 
 def test_insert_coin_and_check():
     m = VendingMachine()
-    run("동전 100")
-    run("동전 100")
+    m.run("동전 100")
+    m.run("동전 100")
     assert "잔액은 200원입니다" == m.run("잔액")
+
+def test_coffee_yes_money():
+    m = VendingMachine()
+    m.run("동전 200")
+    assert "커피가 나왔습니다" == m.run("음료 커피")
+
+
+def test_coffee_no_money():
+    m = VendingMachine()
+    m.run("동전 150")
+    assert "잔액이 부족합니다" == m.run("음료 커피")
--- vm.py
+++ vm.py
@@ -6,7 +6,14 @@ class VendingMachine:
             tokens = raw.split(" ")
             cmd, params = tokens[0], tokens[1:]
 
-            if cmd == "잔액":
+            if cmd == "음료":
+                price = 150
+                if self._change >= price:
+                    self._change = self._change - 150
+                    return "커피가 나왔습니다"
+                else:
+                    return "잔액이 부족합니다"
+            elif cmd == "잔액":
                 return "잔액은 " + str(self._change) + "원입니다"
             elif cmd == "동전":
                 coin = params[0]
